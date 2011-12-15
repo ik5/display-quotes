90b2be103bc8a646dae67e5d5e0edcca0d61ccf7
ik5 <idok@linesip.com>
2011-12-16 00:33:46 +0200

    Starting to work on the configuration unit

--- src/units/untsavesettings.pas
+++ src/units/untsavesettings.pas
@@ -5,9 +5,60 @@ unit untSaveSettings;
 interface
 
 uses
-  Classes, SysUtils; 
+  Classes, SysUtils, jsonConf;
+
+type
+
+  { TSettings }
+
+  TSettings = class
+  private
+    FLastQuote   : Integer;
+    FNotifyEvent : Boolean;
+    FWindow      : TRect;
+    FFile        : String;
+    FConfig      : TJSONConfig;
+  public
+    constructor Create; virtual;
+    destructor Destroy; override;
+    procedure Save;     virtual;
+    procedure Write;    virtual;
+  published
+    property LastQuote   : Integer read FLastQuote   write FLastQuote;
+    property NotifyEvent : Boolean read FNotifyEvent write FNotifyEvent;
+    property Window      : TRect   read FWindow      write FWindow;
+  end;
+
+var
+  ProgramSettings : TSettings;
 
 implementation
+
+{ TSettings }
+
+constructor TSettings.Create;
+var
+  UserDir : String;
+begin
+   UserDir := GetAppConfigDir(False);
+   if not DirectoryExists(UserDir) then
+     ForceDirectories(UserDir);
+end;
+
+destructor TSettings.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TSettings.Save;
+begin
+
+end;
+
+procedure TSettings.Write;
+begin
+
+end;
 
 end.
 
